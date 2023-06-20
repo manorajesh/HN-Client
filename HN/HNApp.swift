192bf3df685cd049c2d4acfe2b281be4c672a053
manorajesh <mano@raje.sh>
2023-06-19 18:15:34 -0700

    Initial Commit

---
+++ HN/HNApp.swift
@@ -0,0 +1,17 @@
+//
+//  HNApp.swift
+//  HN
+//
+//  Created by Mano Rajesh on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct HNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
